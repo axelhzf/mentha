4ff6bb60d4ff05512ab3b7a3ae2ff1174697fb45
Axel Hernández Ferrera <axelhzf@gmail.com>
2018-12-15 11:17:00 +0000

    add styled-components support

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import styled, { createGlobalStyle } from "styled-components/macro";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Mentha
-          </p>
-        </header>
-      </div>
+      <>
+        <GlobalStyle />
+        <Container>
+          <Header>
+            <p>Mentha</p>
+          </Header>
+        </Container>
+      </>
     );
   }
 }
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+  }
+`;
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
 
 export default App;
