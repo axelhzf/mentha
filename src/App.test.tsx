42f4ad83601174dbd15cb5e7e8d63f96b76623d5
Axel Hernández Ferrera <axelhzf@gmail.com>
2018-12-15 11:24:28 +0000

    setup test

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './App';
+import {render} from 'react-testing-library'
+
+it('renders without crashing', () => {
+  const renderResult = render(<App />);
+  renderResult.debug();
+  expect(renderResult.getByText('Mentha')).toBeDefined();
+});
