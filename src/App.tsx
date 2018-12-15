4dff1cb0faa509d86307c70ba420eb69a9f6e9ed
Axel Hernández Ferrera <axelhzf@gmail.com>
2018-12-15 12:07:58 +0000

    extract styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,19 @@
-import React, { Component } from "react";
-import styled, { createGlobalStyle } from "styled-components/macro";
+import React from "react";
+
 import { Canvas } from "./Canvas";
 import { NavBar } from "./NavBar";
 import { Rect } from "./Rect";
+import { GlobalStyle } from "./styled";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <GlobalStyle />
-        <Container>
-          <Canvas>
-            <Rect x={200} y={200} width={200} height={200} />
-            <Rect x={700} y={200} width={50} height={50} />
-          </Canvas>
-          <NavBar />
-        </Container>
-      </>
-    );
-  }
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Canvas>
+        <Rect x={200} y={200} width={200} height={200} />
+        <Rect x={700} y={200} width={50} height={50} />
+      </Canvas>
+      <NavBar />
+    </>
+  );
 }
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
-  }
-`;
-
-const Container = styled.div`
-  text-align: center;
-`;
-
-export default App;
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components/macro";
+import {styled} from "./styled";
 
 export function Canvas(props: { children: React.ReactNode }) {
   return <Container>{props.children}</Container>;
