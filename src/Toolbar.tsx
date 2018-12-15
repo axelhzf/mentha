93f910e15913ac800f0fd09a4121385dc8793529
Axel Hernández Ferrera <axelhzf@gmail.com>
2018-12-15 13:22:50 +0000

    Add minimal toolbar style

--- src/Toolbar.tsx
+++ src/Toolbar.tsx
@@ -10,7 +10,7 @@ export function Toolbar(props: Props) {
   return (
     <ToolbarContainer>
       <ToolbarButton onClick={() => props.dispatch({ type: 'add-rect' })}>
-        Add Rect
+        Rect
       </ToolbarButton>
     </ToolbarContainer>
   );
@@ -19,7 +19,17 @@ export function Toolbar(props: Props) {
 const ToolbarContainer = styled.div`
   position: absolute;
   top: 50px;
-  left: 50px;
+  left: 10px;
+  background: #282c34;
+  color: white;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
 `;
 
-const ToolbarButton = styled.button``;
+const ToolbarButton = styled.button`
+  background: none;
+  border: none;
+  color: white;
+  font-size: 14px;
+  padding: 8px;
+  cursor: pointer;
+`;
